ea48063b824177c440992bd8211d6216d24f9884
Shajal Kumer Sarker <shajalkumer@gmail.com>
2021-07-27 22:39:27 +0600

    Updates

---
+++ precache-manifest.4cd6fc16e8512f26ff1a1928453f297e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a5e0d049b69dab05ca09a70d0d1092",
+    "url": "/react-covid19-app/index.html"
+  },
+  {
+    "revision": "d005bd6bdce603c51d44",
+    "url": "/react-covid19-app/static/css/main.6967bffd.chunk.css"
+  },
+  {
+    "revision": "5e7ba961a9b0a94a566b",
+    "url": "/react-covid19-app/static/js/2.9d0194b5.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/react-covid19-app/static/js/2.9d0194b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d005bd6bdce603c51d44",
+    "url": "/react-covid19-app/static/js/main.2df4992f.chunk.js"
+  },
+  {
+    "revision": "45369e10cf68d8d07a53",
+    "url": "/react-covid19-app/static/js/runtime-main.8ba0a928.js"
+  },
+  {
+    "revision": "7786bbd41b0ab3b8fdc2d472789e306e",
+    "url": "/react-covid19-app/static/media/covid-19.7786bbd4.png"
+  }
+]);
